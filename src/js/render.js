b21cbc022500ac2565e9d336776b6ee7ae07e032
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-10-04 18:01:11 +0300

    go on

--- src/js/render.js
+++ src/js/render.js
@@ -1,23 +1,18 @@
 import { 
         toShowModalWin,
         toCloseModal
- } from './madal.js';
+ }                              from './madal.js';
 
 const galleryRef = document.querySelector('.gallery');
 const section = document.querySelector('.section');
 const body = document.querySelector('body');
 
-// console.log(galleryRef);
-// console.log(modal);
-// console.log(cross);
-
 export function renderLayout(arr){
     const ren = arr.map(({poster_path, id}, idx) =>{
     return `
         <li class="card" id=${id}>
+            <img src="https://image.tmdb.org/t/p/w500${poster_path}" width="" alt="">
             <h2>${id}</h2>
-           <img src="https://image.tmdb.org/t/p/w500${poster_path}" width="" alt="">
-           
         </li>                                              
         `
 }).join('');
@@ -28,8 +23,8 @@ export function renderLayout(arr){
  galleryRef.addEventListener('click', modalShow);
                                                     function modalShow(evt){
                                                         window.addEventListener('keydown', (evt)=>{
-                                                            if(evt.code === 'Escape'){
-                                                            toCloseModal();
+                                                        if(evt.code === 'Escape'){
+                                                        toCloseModal();
                                                         }
                                                     });
    const nestedElem = evt.target;
