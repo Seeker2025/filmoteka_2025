8d25e09119195f91b3e368b06dcafe9e6e3bc5fa
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2026-01-05 09:46:34 +0000

    Deploying to gh-pages from @ Seeker2025/filmoteka_2025@66a52aecd3c502acf51f49ce06de234cbbe58d17 🚀

--- assets/toFun-ON7hnRB9.js
+++ assets/toFun-ON7hnRB9.js
@@ -1,6 +1,6 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const r of e.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&a(r)}).observe(document,{childList:!0,subtree:!0});function c(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function a(t){if(t.ep)return;t.ep=!0;const e=c(t);fetch(t.href,e)}})();const p=document.querySelector(".icon_yellow_js");function A(){setInterval(()=>{p.classList.toggle("icon_scale")},700)}A();const g="/filmoteka_2025/assets/no_img02-C_eq9g-1.png",f="/filmoteka_2025/assets/noth-DYALNzd5.png",y=document.querySelector(".gallery");console.log(y);const b=document.querySelector("ul.gallery");console.log(b);function _(s,o){if(s.length){const c=s.map(({poster_path:a,id:t,title:e,release_date:r},d)=>{let i=`<img class="img_of_card" src="https://image.tmdb.org/t/p/w500${a}" alt="${e}">`;return a||(i=`<img class="img_of_card" src=${g} alt="No image">`),`
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const r of e.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&a(r)}).observe(document,{childList:!0,subtree:!0});function c(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function a(t){if(t.ep)return;t.ep=!0;const e=c(t);fetch(t.href,e)}})();const p=document.querySelector(".icon_yellow_js");function A(){setInterval(()=>{p.classList.toggle("icon_scale")},700)}A();const g="/filmoteka_2025/assets/no_img02-C_eq9g-1.png",f="/filmoteka_2025/assets/noth-DYALNzd5.png",y=document.querySelector(".gallery");console.log(y);const b=document.querySelector("ul.gallery");console.log(b);function _(s,o){if(s.length){const c=s.map(({poster_path:a,id:t,title:e,release_date:r},d)=>{let l=`<img class="img_of_card" src="https://image.tmdb.org/t/p/w500${a}" alt="${e}">`;return a||(l=`<img class="img_of_card" src=${g} alt="No image">`),`
                             <li class="card card_js" id=${t}>
-                            ${i}
+                            ${l}
      <div class="card_box">
          <h1 class="card_title">${e}</h1>
          <h2 class="card_jenre">Drama, Comedy | ${Number.parseInt(r)||"No date"}</h2>
@@ -10,7 +10,7 @@
                              <li class="card card_js">
                              <img class="img_of_card" src=${f} alt="No image">
                              </li>    
-                                     `;o.innerHTML=c}}const h="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAB6SURBVHgB7ZVBCsAgDASXvmR/aH/cp1gPHkqRmjRqEDLgRdcMiBogCIKFnGVQkWfdY5bmMi7I5KzZDKOcj0I9uSY7TC7JDJd/rU2Tt+amwJdoibQl/y09sAmEw1ETDpeLcHhOhMMHQkVBTbaLpUkkGEnQt0WzNAgCMTf64Es4we8RJwAAAABJRU5ErkJggg==";function v(s,o){if(!s)return;const{poster_path:c,title:a,popularity:t,vote_average:e,vote_count:r,overview:d,genre_ids:i}=s;let u=`<img class="img_modal" src="https://image.tmdb.org/t/p/w500${c}" alt="${a}">`;c||(u=`<img src=${g} alt="No image">`);const m=`<div class="backdrop">
+                                     `;o.innerHTML=c}}const h="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAB6SURBVHgB7ZVBCsAgDASXvmR/aH/cp1gPHkqRmjRqEDLgRdcMiBogCIKFnGVQkWfdY5bmMi7I5KzZDKOcj0I9uSY7TC7JDJd/rU2Tt+amwJdoibQl/y09sAmEw1ETDpeLcHhOhMMHQkVBTbaLpUkkGEnQt0WzNAgCMTf64Es4we8RJwAAAABJRU5ErkJggg==";function v(s,o){if(!s)return;const{poster_path:c,title:a,popularity:t,vote_average:e,vote_count:r,overview:d,genre_ids:l}=s;let u=`<img class="img_modal" src="https://image.tmdb.org/t/p/w500${c}" alt="${a}">`;c||(u=`<img src=${g} alt="No image">`);const m=`<div class="backdrop">
                 <div class = "modal">
                         <div class="cross">
                         
@@ -65,7 +65,7 @@
                                         </td>
 
                                         <td class="table_two">
-                                                ${i[0],i[1]}
+                                                ${l[0],l[1]}
                                         </td>
 
                                 </tr>
@@ -78,9 +78,11 @@
                                         <td colspan = "2">ABOUT</td>
                                 </tr>
 
-                                <tr class="description">
+                                <tr>
         <td colspan = "2">
+                        <div class="description">
         ${d||"Unfortunately, this film does not have a description yet."}
+                        </div>
         </td>
                                 </tr>
                       
@@ -99,5 +101,5 @@
                      
                 </div>
         </div>   
-                `;return o.innerHTML=m}function w(s,o,c){window.addEventListener("keyup",r=>{r.code==="Escape"&&e(s)});const a=document.querySelector("div.cross"),t=document.querySelector(".backdrop");t.addEventListener("click",r=>{console.log(r.target),r.target===t&&(console.log("whoa!"),e(s))}),a.addEventListener("click",()=>{e(s),a.removeEventListener("click",()=>{}),window.removeEventListener("keyup",()=>{}),o.removeEventListener("click",()=>{}),c.removeEventListener("click",()=>{})});function e(r){r.innerHTML=""}}function S(s,o){const c=s.closest(".card"),a=Number(c.getAttribute("id"));return JSON.parse(localStorage.getItem(o)).find(r=>r.id===a)}let n=JSON.parse(localStorage.getItem("watched"))??[],l=JSON.parse(localStorage.getItem("queued"))??[];function L(s){n.some(o=>o.id===s.id)?(n=n.filter(o=>o.id!==s.id),localStorage.setItem("watched",JSON.stringify(n))):(n.push(s),localStorage.setItem("watched",JSON.stringify(n)))}function E(s){l.some(o=>o.id===s.id)?(l=l.filter(o=>o.id!==s.id),localStorage.setItem("queued",JSON.stringify(l))):(l.push(s),localStorage.setItem("queued",JSON.stringify(l)))}export{L as a,E as b,w as c,v as m,_ as r,S as t};
-//# sourceMappingURL=toFun-CRhCGtP3.js.map
+                `;return o.innerHTML=m}function w(s,o,c){window.addEventListener("keyup",r=>{r.code==="Escape"&&e(s)});const a=document.querySelector("div.cross"),t=document.querySelector(".backdrop");t.addEventListener("click",r=>{console.log(r.target),r.target===t&&(console.log("whoa!"),e(s))}),a.addEventListener("click",()=>{e(s),a.removeEventListener("click",()=>{}),window.removeEventListener("keyup",()=>{}),o.removeEventListener("click",()=>{}),c.removeEventListener("click",()=>{})});function e(r){r.innerHTML=""}}function S(s,o){const c=s.closest(".card"),a=Number(c.getAttribute("id"));return JSON.parse(localStorage.getItem(o)).find(r=>r.id===a)}let n=JSON.parse(localStorage.getItem("watched"))??[],i=JSON.parse(localStorage.getItem("queued"))??[];function L(s){n.some(o=>o.id===s.id)?(n=n.filter(o=>o.id!==s.id),localStorage.setItem("watched",JSON.stringify(n))):(n.push(s),localStorage.setItem("watched",JSON.stringify(n)))}function E(s){i.some(o=>o.id===s.id)?(i=i.filter(o=>o.id!==s.id),localStorage.setItem("queued",JSON.stringify(i))):(i.push(s),localStorage.setItem("queued",JSON.stringify(i)))}export{L as a,E as b,w as c,v as m,_ as r,S as t};
+//# sourceMappingURL=toFun-ON7hnRB9.js.map
