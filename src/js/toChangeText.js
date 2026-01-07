f3109ab8f5efd23c341ff5a188e53d21a92913cf
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-01-07 23:20:27 +0200

    fix

--- src/js/toChangeText.js
+++ src/js/toChangeText.js
@@ -1,5 +1,5 @@
 export function toChangeTxtOnBtn(btn, obj, key, txtRem, txtAdd){
-     const arr = JSON.parse(localStorage.getItem(key));
+     const arr = JSON.parse(localStorage.getItem(key)) ?? [];
                         const present = arr.some(itm => itm.id === obj.id);
                         if(present){
                             btn.textContent = txtRem;
