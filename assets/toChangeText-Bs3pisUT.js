bdd211cf78616234633dfe9268aae19bdef651f0
Seeker2025 <85400723+Seeker2025@users.noreply.github.com>
2026-02-13 07:13:38 +0000

    Deploying to gh-pages from @ Seeker2025/filmoteka_2025@b9f0f23f1551e4210c16d548ee42fc305c604578 🚀

--- assets/toChangeText-Bs3pisUT.js
+++ assets/toChangeText-Bs3pisUT.js
@@ -2,7 +2,7 @@ import{a as _}from"./vendor-D4Zk_CBc.js";(function(){const r=document.createElem
                              <li class="card card_js card_dark card_light">
                              <img class="img_of_card" src=${N} alt="404">
                              </li>    
-                                     `;r.innerHTML=t}if(e?.length){const t=e.map(({poster_path:o,id:s,title:a,release_date:i,genre_ids:n},d)=>{let l=`<img class="img_of_card" src="https://image.tmdb.org/t/p/w500${o}" alt="${a}">`;return o||(l=`<img class="img_of_card" src=${p} alt="No image">`),`
+                                     `;r.innerHTML=t}if(e?.length){const t=e.map(({poster_path:o,id:s,title:a,release_date:i,genre_ids:n},g)=>{let l=`<img class="img_of_card" src="https://image.tmdb.org/t/p/w500${o}" alt="${a}">`;return o||(l=`<img class="img_of_card" src=${p} alt="No image">`),`
                             <li class="card card_js card_dark card_light" id=${s}>
                             ${l}
      <div class="card_box">
@@ -14,13 +14,13 @@ import{a as _}from"./vendor-D4Zk_CBc.js";(function(){const r=document.createElem
                              <li class="card card_js card_dark card_light">
                              <img class="img_of_card" src=${I} alt="No image">
                              </li>    
-                                     `;r.innerHTML=t}}const v="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAB6SURBVHgB7ZVBCsAgDASXvmR/aH/cp1gPHkqRmjRqEDLgRdcMiBogCIKFnGVQkWfdY5bmMi7I5KzZDKOcj0I9uSY7TC7JDJd/rU2Tt+amwJdoibQl/y09sAmEw1ETDpeLcHhOhMMHQkVBTbaLpUkkGEnQt0WzNAgCMTf64Es4we8RJwAAAABJRU5ErkJggg==",w="Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYWY1ZmQwYjgzOGJmMmEyNTlmZjA2Y2I3NTk2ODAzNyIsIm5iZiI6MTY3MDIyNjI2NC4xMzIsInN1YiI6IjYzOGRhMTU4MTI4M2U5MDA5NzY3Njg3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LbbNBAE2uu7qSjmBFjXYtTYw99nAa-BxDgIdgH9cD08";async function L(e,r){try{const t=await _.get(`${e}movie/${r}/videos`,{headers:{"Content-Type":"application/json",Authorization:w}});return console.log(t.data),t.data}catch(t){console.log(t)}}function S(e,r){e.innerHTML=`
+                                     `;r.innerHTML=t}}const v="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAB6SURBVHgB7ZVBCsAgDASXvmR/aH/cp1gPHkqRmjRqEDLgRdcMiBogCIKFnGVQkWfdY5bmMi7I5KzZDKOcj0I9uSY7TC7JDJd/rU2Tt+amwJdoibQl/y09sAmEw1ETDpeLcHhOhMMHQkVBTbaLpUkkGEnQt0WzNAgCMTf64Es4we8RJwAAAABJRU5ErkJggg==",w="Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYWY1ZmQwYjgzOGJmMmEyNTlmZjA2Y2I3NTk2ODAzNyIsIm5iZiI6MTY3MDIyNjI2NC4xMzIsInN1YiI6IjYzOGRhMTU4MTI4M2U5MDA5NzY3Njg3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LbbNBAE2uu7qSjmBFjXYtTYw99nAa-BxDgIdgH9cD08";async function L(e,r){try{const t=await _.get(`${e}movie/${r}/videos`,{headers:{"Content-Type":"application/json",Authorization:w}});return console.log(t.data),console.log(t.data.results[0]?.official),t.data}catch(t){console.log(t)}}function S(e,r){e.innerHTML=`
             <iframe  class="iframe"
             src="https://www.youtube.com/embed/${r}?autoplay=1"
             frameborder="0"
             allowfullscreen>
             </iframe>
-                   `}const T="https://api.themoviedb.org/3/";let c=null;function M(e,r){if(JSON.parse(localStorage.getItem("ui"))==="dark"?c="dark":c="light",!e)return;const{poster_path:t,title:o,popularity:s,vote_average:a,vote_count:i,overview:n,genre_ids:d,id:l}=e;let g=`<img class="img_modal" src="https://image.tmdb.org/t/p/w500${t}" alt="${o}">`;t||(g=`<img src=${p} alt="No image">`);const A=`<div class="backdrop">
+                   `}const T="https://api.themoviedb.org/3/";let c=null;function M(e,r){if(JSON.parse(localStorage.getItem("ui"))==="dark"?c="dark":c="light",!e)return;const{poster_path:t,title:o,popularity:s,vote_average:a,vote_count:i,overview:n,genre_ids:g,id:l}=e;let u=`<img class="img_modal" src="https://image.tmdb.org/t/p/w500${t}" alt="${o}">`;t||(u=`<img src=${p} alt="No image">`);const A=`<div class="backdrop">
                 <div class = "modal ${c}">
                         <div class="cross">
                         
@@ -31,7 +31,7 @@ import{a as _}from"./vendor-D4Zk_CBc.js";(function(){const r=document.createElem
                         </div>
 
                         <picture class="picture">
-                                ${L(T,l).then(({results:u})=>{const m=document.querySelector(".picture");if(!u?.length)m.innerHTML=g;else{let h=u[0].key;S(m,h)}})}
+                                ${L(T,l).then(({results:d})=>{const m=document.querySelector(".picture");if(!d?.length||!d[0]?.official)m.innerHTML=u;else{let h=d[0].key;S(m,h)}})}
                         </picture>                       
                             
                 <table>
@@ -77,7 +77,7 @@ import{a as _}from"./vendor-D4Zk_CBc.js";(function(){const r=document.createElem
                                     </td>
 
                                     <td class="table_two">
-                                        ${f(d).join(", ")||"No genres yet"}
+                                        ${f(g).join(", ")||"No genres yet"}
                                     </td>
 
                                 </tr>
@@ -114,4 +114,4 @@ import{a as _}from"./vendor-D4Zk_CBc.js";(function(){const r=document.createElem
                 </div>
         </div>   
                 `;r.innerHTML=A}function O(e,r,t){window.addEventListener("keyup",i=>{i.code==="Escape"&&a(e)});const o=document.querySelector("div.cross"),s=document.querySelector(".backdrop");s.addEventListener("click",i=>{i.target===s&&a(e)}),o.addEventListener("click",()=>{a(e),o.removeEventListener("click",()=>{}),window.removeEventListener("keyup",()=>{}),r.removeEventListener("click",()=>{}),t.removeEventListener("click",()=>{})});function a(i){i.innerHTML=""}}function E(e,r){const t=e.closest(".card"),o=Number(t.getAttribute("id")),s=JSON.parse(localStorage.getItem(r));return s?s.find(i=>i.id===o):void 0}function J(e,r){let t=JSON.parse(localStorage.getItem(r))??[];t.some(o=>o.id===e.id)?(t=t.filter(o=>o.id!==e.id),localStorage.setItem(r,JSON.stringify(t))):(t.push(e),localStorage.setItem(r,JSON.stringify(t)))}console.log("switch");function j(e,r,t){e.classList.toggle("light"),r.classList.toggle("con_light"),t&&t.classList.toggle("input_light")}function B(e,r){r&&e.forEach(t=>{t.classList.toggle("card_light")})}function C(e,r,t,o,s){(JSON.parse(localStorage.getItem(t))??[]).some(n=>n.id===r.id)?e.textContent=o:e.textContent=s}export{C as a,J as b,O as c,j as d,B as e,M as m,k as r,E as t};
-//# sourceMappingURL=toChangeText-JPbgLij-.js.map
+//# sourceMappingURL=toChangeText-Bs3pisUT.js.map
